50a00d6da3bc1f48fa67e2fa1dc686f4394b22e9
YannMassoumou <YannPC>
2026-03-02 17:17:17 +0100

    completed task1

---
+++ WeatherApp.Core/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public interface IWeatherService
+    {
+        Task<WeatherResult> GetWeatherAsync(string city, string country, CancellationToken cancellationToken = default);
+    }
+}
---
+++ WeatherApp.Core/WeatherResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class WeatherResult
+    {
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public double TemperatureCelsius { get; set; }
+        public string? Description { get; set; }
+    }
+}
